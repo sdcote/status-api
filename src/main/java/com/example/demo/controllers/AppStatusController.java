ddca3389fbcb87af8efc2ec916b18961d0752cd0
Cote <Steve.Cote@insight.com>
2019-12-16 10:58:24 -0500

    Added support for statically defined version.

--- src/main/java/com/example/demo/controllers/AppStatusController.java
+++ src/main/java/com/example/demo/controllers/AppStatusController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,19 +12,42 @@ import java.util.HashMap;
 import java.util.Map;
 
 
+/**
+ * Proof of Concept only requires status. A more complete service will include Version and deployment information such
+ * as hostname on which the instance is running to support additional use cases of recovering partial deployments and
+ * verifying loa balancing.
+ */
 @RestController
 public class AppStatusController {
 
     private static final String STATUS = "status";
+    private static final String VERSION = "version";
+    private static final String UNKNOWN = "unknown";
+
     String state = "stand-by";
 
+    @Value("${appversion}")
+    private String version;
+
+
     private String getState() {
-        return state;
+        if (StringUtils.isEmpty(state))
+            return UNKNOWN;
+        else
+            return state;
+    }
+
+    private String getVersion() {
+        if (StringUtils.isEmpty(version))
+            return UNKNOWN;
+        else
+            return version;
     }
 
     private Map<String, String> getStatusMap() {
         Map<String, String> retval = new HashMap<>();
         retval.put(STATUS, getState());
+        retval.put(VERSION, getVersion());
         return retval;
     }
 
