6d033c6f41e2176d2871b2c5bfa576e0fb28d719
sdcote <sdcote@aep.com>
2019-12-17 16:26:53 -0500

    fixed imports

--- src/main/java/com/example/demo/AppState.java
+++ src/main/java/com/example/demo/AppState.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.util.StringUtils;
 
 import java.io.File;
